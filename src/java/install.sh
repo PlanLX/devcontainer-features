62270778c7a229bca279d5c3e929f757865570a6
PlanLX <sloudy@gmail.com>
2024-05-23 13:12:08 +0000

    240523 # update

--- src/java/install.sh
+++ src/java/install.sh
@@ -56,10 +56,7 @@ clean_up() {
 }
 clean_up
 
-# Ensure that login shells get the correct path if the user updated the PATH using ENV.
-rm -f /etc/profile.d/00-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
-chmod +x /etc/profile.d/00-restore-env.sh
+
 
 # Determine the appropriate non-root user
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
@@ -122,38 +119,84 @@ check_packages() {
     esac
 }
 
-# Use Microsoft JDK for everything but JDK 8 and 18 (unless specified differently with jdkDistro option)
-get_jdk_distro() {
-    VERSION="$1"
-    if [ "${JDK_DISTRO}" = "ms" ]; then
-        if echo "${VERSION}" | grep -E '^8([\s\.]|$)' > /dev/null 2>&1 || echo "${VERSION}" | grep -E '^18([\s\.]|$)' > /dev/null 2>&1; then
-            JDK_DISTRO="tem"
-        fi
-    fi
+getFile() {
+  if [ $# -ne 2 ]; then
+    echo "getFile takes 2 arguments, $# argument(s) given"
+    echo 'Usage: getFile https://example.com file_name'
+    exit 1;
+  elif command -v wget &> /dev/null; then
+    wget "$1" -O "$2"
+  elif command -v curl &> /dev/null; then
+    curl -s "$1" -o "$2"
+  else
+    echo 'Please install wget or curl to continue'
+    exit 1;
+  fi
 }
 
 
-export DEBIAN_FRONTEND=noninteractive
 
+# Use Eclipse Temurin™ OpenJDK binaries
+#  https://api.adoptium.net/v3/info/available_releases
+
+# shellcheck disable=SC2002 # Disable UUOC error
+setJDKVars() {
+  getFile https://api.adoptium.net/v3/info/available_releases available_releases
+  JDK_MAX=$(cat available_releases \
+      | grep 'tip_version' \
+      | cut -d':' -f 2 \
+      | sed 's/,//g; s/ //g')
+  JDK_GA=$(cat available_releases \
+          | grep 'most_recent_feature_release' \
+          | cut -d':' -f 2 \
+          | sed 's/,//g; s/ //g')
+  rm available_releases
+}
+
+# printJDK() {
+
+#   mkdir -p /usr/lib/jvm/jdk$JAVA_VERSION
+#   wget 'https://api.adoptium.net/v3/binary/latest/$JAVA_VERSION/ga/linux/x64/jdk/hotspot/normal/adoptium?project=jdk' -O - | tar xzf - -C /usr/lib/jvm/jdk$JAVA_VERSION
+
+
+# }
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+
+
+export DEBIAN_FRONTEND=noninteractive
+# 取类型 
+arch="$(dpkg --print-architecture)"
 architecture="$(uname -m)"
 if [ "${architecture}" != "amd64" ] && [ "${architecture}" != "x86_64" ] && [ "${architecture}" != "arm64" ] && [ "${architecture}" != "aarch64" ]; then
     echo "(!) Architecture $architecture unsupported"
     exit 1
 fi
 
+
+
+
 # Install dependencies,
 check_packages ca-certificates zip unzip sed findutils util-linux tar
-check_packages openjdk-8-jdk
-# Make sure passwd (Debian) and shadow-utils RHEL family is installed
-if [ ${ADJUSTED_ID} = "debian" ]; then
-    check_packages passwd
-fi
-# minimal RHEL installs may not include curl, or includes curl-minimal instead.
-# Install curl if the "curl" command is not present.
-if ! type curl > /dev/null 2>&1; then
-    check_packages curl
-fi
+# check_packages openjdk-8-jdk
 
+# Install OpenJDK
+if [ -n "${JAVA_VERSION}" ]; then    
+    JDK_HOME="/usr/lib/jvm/jdk$JAVA_VERSION"
+    JAVA_HOME="/usr/lib/jvm/jdk"
+    mkdir -p "$JDK_HOME"
+    getFile "https://api.adoptium.net/v3/binary/latest/${JAVA_VERSION}/ga/linux/x64/jdk/hotspot/normal/adoptium?project=jdk" - | tar xzf - -C "$JDK_HOME"  --strip-components=1
+    
+fi
+ln -sf ${JDK_HOME} ${JAVA_HOME}
+ln -sf ${JDK_HOME}/bin/java /usr/bin/java
+ln -sf ${JDK_HOME}/bin/javac /usr/bin/javac
+ln -sf ${JDK_HOME}/bin/keytool /usr/bin/keytool
+echo "export JAVA_HOME=${JAVA_HOME}" >> /etc/profile.d/00-restore-env.sh
 
 # Install Ant
 if [[ "${INSTALL_ANT}" = "true" ]] && ! ant -version > /dev/null 2>&1; then
@@ -163,11 +206,20 @@ fi
 # Install Gradle
 if [[ "${INSTALL_GRADLE}" = "true" ]] && ! gradle --version > /dev/null 2>&1; then
     check_packages gradle
+    
 fi
 
 # Install Maven
 if [[ "${INSTALL_MAVEN}" = "true" ]] && ! mvn --version > /dev/null 2>&1; then
-    check_packages maven
+    # https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+    mkdir -p /usr/share/maven /usr/share/maven/ref
+    getFile "https://dlcdn.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz" /tmp/maven
+    tar xzvf /tmp/maven -C /usr/share/maven --strip-components=1
+    ln -sf /usr/share/maven/bin/mvn /usr/bin/mvn
+    echo "export MAVEN_HOME=/usr/share/maven" >> /etc/profile.d/00-restore-env.sh
+    echo "export MAVEN_CONFIG=/root/.m2" >> /etc/profile.d/00-restore-env.sh
+    rm -f /tmp/maven 
+    # COPY maven-settings.xml /usr/share/maven/ref/
 fi
 
 # Install Groovy
