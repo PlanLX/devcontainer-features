69626d6a16931391f36561b8adf69cbd4b0da04f
PlanLX <sloudy@gmail.com>
2024-05-23 02:27:52 +0000

    240523

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-npm install -g @devcontainers/cli
+# 加载环境变量
+# ls -la
+# cat ./.devcontainer/.env
+if [ -f ./.devcontainer/.env ]; then
+  # 从 .env 文件加载环境变量
+  export $(grep -v '^#' ./.devcontainer/.env | xargs)
+fi
 
-pipx install shfmt-py
+# 启动你的应用程序
+exec "$@"
 
+
+# npm install -g @devcontainers/cli
+
+# pipx install shfmt-py
+# 
 # this will add hover annotations in shell script files, assuming mads-hartmann.bash-ide-vscod is installed
-docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
+# docker container run --name explainshell --restart always -p 5000:5000 -d spaceinvaderone/explainshell
+
